3012b2d3546bf533f1c77142e8da6d02a51a48bf
Pooja Gawas <pooja_gawas@bitbucket.org>
2020-12-04 18:18:45 +0530

    site report fields

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/site-report.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/site-report.js
@@ -162,11 +162,6 @@ const propertyInfo = () => ({
       ownedByLabel, {
         jsonPath: "Applications[0].applicationDetails.owner.transferorDetails.ownerName"
       }
-    ),
-    so: getLabelWithValue(
-      soLabel, {
-        jsonPath: "Applications[0].applicationDetails.owner.transferorDetails.guardianName"
-      }
     )
   })
 })
@@ -178,10 +173,483 @@ const header = getCommonHeader({
   labelKey: "ES_SITE_REPORT_HEADER"
 });
 
+const particularsOfHouseField = {
+  label: {
+    labelName: "Particular of House/Shop in question",
+    labelKey: "ES_PARTICULARS_OF_HOUSE_OR_SHOP_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter particular of House/Shop in question",
+    labelKey: "ES_PARTICULARS_OF_HOUSE_OR_SHOP_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.particularsOfHouse"
+}
+
+const houseAreaField = {
+  label: {
+    labelName: "Shop/House falls in which area (Abadi/ Lal Dora/ Acquired/ Un acquired Ousters Abadi/ Other",
+    labelKey: "ES_HOUSE_AREA_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Shop/House falls in which area",
+    labelKey: "ES_HOUSE_AREA_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.houseArea"
+}
+
+const ownershipField = {
+  label: {
+    labelName: "Ownership",
+    labelKey: "ES_OWNERSHIP_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Ownership",
+    labelKey: "ES_OWNERSHIP_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.ownership"
+}
+
+const possessionField = {
+  label: {
+    labelName: "Possession",
+    labelKey: "ES_POSSESSION_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Possession",
+    labelKey: "ES_POSSESSION_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.possession"
+}
+
+const modeOfOwnershipField = {
+  label: {
+    labelName: "How become the owner (by way of sale deed/ WILL/ Ancestral/ Other)",
+    labelKey: "ES_MODE_OF_OWNERSHIP_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter how become the owner",
+    labelKey: "ES_MODE_OF_OWNERSHIP_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.modeOfOwnership"
+}
+
+const heightField = {
+  label: {
+    labelName: "Height",
+    labelKey: "ES_HEIGHT_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Height",
+    labelKey: "ES_HEIGHT_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.height"
+}
+
+const cantileverField = {
+  label: {
+    labelName: "Cantilever",
+    labelKey: "ES_CANTILEVER_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Cantilever",
+    labelKey: "ES_CANTILEVER_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.cantilever"
+}
+
+const noOfFloorsField = {
+  label: {
+    labelName: "No. of floors",
+    labelKey: "ES_NUMBER_OF_FLOORS_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter No. of floors",
+    labelKey: "ES_NUMBER_OF_FLOORS_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.noOfFloors"
+}
+
+const encroachmentField = {
+  label: {
+    labelName: "Encroachment adjoining public land (if any)",
+    labelKey: "ES_ENCROACHMENT_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Encroachment adjoining public land (if any)",
+    labelKey: "ES_ENCROACHMENT_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.encroachment"
+}
+
+const violationField = {
+  label: {
+    labelName: "Any other violation",
+    labelKey: "ES_VIOLATION_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter any other violation",
+    labelKey: "ES_VIOLATION_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.violation"
+}
+
+const dimensionOfBuildingField = {
+  label: {
+    labelName: "Dimension of the building/site",
+    labelKey: "ES_DIMENSION_OF_BUILDING_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Dimension of the building/site",
+    labelKey: "ES_DIMENSION_OF_BUILDING_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.dimensionOfBuilding"
+}
+
+const areaField = {
+  label: {
+    labelName: "Area",
+    labelKey: "ES_AREA_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Area",
+    labelKey: "ES_AREA_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.area"
+}
+
+const excessAreaField = {
+  label: {
+    labelName: "Excess Area",
+    labelKey: "ES_EXCESS_AREA_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Excess Area",
+    labelKey: "ES_EXCESS_AREA_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.excessArea"
+}
+
+const isCommercialActivityRadioButton = {
+  uiFramework: "custom-containers",
+  componentPath: "RadioGroupContainer",
+  gridDefination: {
+    xs: 12,
+    sm: 6,
+  },
+  jsonPath: "Applications[0].applicationDetails.siteReport.isCommercialActivity",
+  props: {
+    label: {
+      name: "Commercial Activity?",
+      key: "ES_IS_COMMERCIAL_ACTIVITY_LABEL"
+    },
+    buttons: [{
+        labelName: "Yes",
+        labelKey: "ES_COMMON_YES",
+        value: true,
+      },
+      {
+        label: "No",
+        labelKey: "ES_COMMON_NO",
+        value: false,
+      }
+    ],
+    jsonPath: "Applications[0].applicationDetails.siteReport.isCommercialActivity",
+    required: true,
+  },
+  required: true,
+  type: "array",
+  afterFieldChange: (action, state, dispatch) => {
+    // dispatch(
+    //   handleField(
+    //     action.screenKey,
+    //     "components.div.children.detailsContainer.children.nocVerificationDetails.children.cardContent.children.detailsContainer.children.sizeOfAreaPurchased",
+    //     "props.disabled",
+    //     (action.value != "false")
+    //   )
+    // )
+  }
+};
+
+const commercialActivityAreaField = {
+  label: {
+    labelName: "Commercial Activity Area",
+    labelKey: "ES_COMMERCIAL_ACTIVITY_AREA_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Commercial Activity Area",
+    labelKey: "ES_COMMERCIAL_ACTIVITY_AREA_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  visible: false,
+  // required: true,
+  // minLength: 5,
+  // maxLength: 250,
+  jsonPath: "Applications[0].applicationDetails.siteReport.commercialActivityArea"
+}
+
+const dairyOrKeepingCattleRadioButton = {
+  uiFramework: "custom-containers",
+  componentPath: "RadioGroupContainer",
+  gridDefination: {
+    xs: 12,
+    sm: 6,
+  },
+  jsonPath: "Applications[0].applicationDetails.siteReport.dairyOrKeepingCattle",
+  props: {
+    label: {
+      name: "Running diary or keeping cattle ?",
+      key: "ES_DAIRY_OR_KEEPING_CATTLE_LABEL"
+    },
+    buttons: [{
+        labelName: "Running Dairy",
+        labelKey: "ES_COMMON_RUNNING_DAIRY",
+        value: true,
+      },
+      {
+        label: "Keeping Cattle",
+        labelKey: "ES_COMMON_KEEPING_CATTLE",
+        value: false,
+      }
+    ],
+    jsonPath: "Applications[0].applicationDetails.siteReport.runningDairy",
+    required: true,
+  },
+  required: true,
+  type: "array"
+};
+
+const developmentChargesField = {
+  label: {
+    labelName: "Development charges if any (with rate per sq. ft.)",
+    labelKey: "ES_DEVELOPMENT_CHARGES_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Development charges",
+    labelKey: "ES_DEVELOPMENT_CHARGES_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.developmentCharges"
+}
+
+const compositionFeesField = {
+  label: {
+    labelName: "Composition fees if any (with rate per sq. yds.)",
+    labelKey: "ES_COMPOSITION_FEES_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Composition fees",
+    labelKey: "ES_OMPOSITION_FEES_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.compositionFees"
+}
+
+const compositionFeesUnauthorizedField = {
+  label: {
+    labelName: "Composition fee for unauthorized construction if any (with per sq. ft.)",
+    labelKey: "ES_COMPOSITION_FEES_UNAUTHORIZED_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter Composition fees for unauthorized construction if any (with per sq. ft.)",
+    labelKey: "ES_COMPOSITION_FEES_UNAUTHORIZED_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.compositionFeeUnauthorized"
+}
+
+const conversionChargesField = {
+  label: {
+    labelName: "Conversion charges if any (with rate per sq. yds.)",
+    labelKey: "ES_CONVERSION_CHARGES_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter conversion charges if any (with rate per sq. yds.)",
+    labelKey: "ES_CONVERSION_CHARGES_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.conversionCharges"
+}
+
+const totalChargesField = {
+  label: {
+    labelName: "Total charges",
+    labelKey: "ES_TOTAL_CHARGES_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter total charges",
+    labelKey: "ES_TOTAL_CHARGES_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.totalChanges"
+}
+
+const dateOfInspectionField = {
+  label: {
+    labelName: "Date of Inspection",
+    labelKey: "ES_DATE_OF_INSPECTION_LABEL"
+  },
+  placeholder: {
+    labelName: "Enter date of inspection",
+    labelKey: "ES_DATE_OF_INSPECTION_PLACEHOLDER"
+  },
+  gridDefination: {
+    xs: 12,
+    sm: 6
+  },
+  required: true,
+  minLength: 2,
+  maxLength: 50,
+  jsonPath: "Applications[0].applicationDetails.siteReport.dateOfInspection"
+}
 
 export const siteReportDetails = getCommonCard({
   detailsContainer: getCommonContainer({
-    
+    particularsOfHouse: getTextField(particularsOfHouseField),
+    houseArea: getTextField(houseAreaField),
+    ownership: getTextField(ownershipField),
+    possession: getTextField(possessionField),
+    modeOfOwnershipField: getTextField(modeOfOwnershipField),
+    height: getTextField(heightField),
+    cantilever: getTextField(cantileverField),
+    noOfFloors: getTextField(noOfFloorsField),
+    encroachment: getTextField(encroachmentField),
+    violation: getTextField(violationField),
+    dimensionOfBuilding: getTextField(dimensionOfBuildingField),
+    area: getTextField(areaField),
+    excessArea: getTextField(excessAreaField),
+    isCommercialActivity: isCommercialActivityRadioButton,
+    commercialActivityArea: getTextField(commercialActivityAreaField),
+    runningDairy: dairyOrKeepingCattleRadioButton,
+    developmentCharges: getTextField(developmentChargesField),
+    compositionFees: getTextField(compositionFeesField),
+    compositionFeeUnauthorized: getTextField(compositionFeesUnauthorizedField),
+    conversionCharges: getTextField(conversionChargesField),
+    totalChanges: getTextField(totalChargesField),
+    dateOfInspection: getDateField(dateOfInspectionField)
   })
 })
 
