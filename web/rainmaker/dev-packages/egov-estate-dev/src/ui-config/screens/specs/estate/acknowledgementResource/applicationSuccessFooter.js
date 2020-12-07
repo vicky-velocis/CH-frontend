34722200de618264215020c38673d4d5c577fcb2
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-12-07 18:25:58 +0530

    PE-627 bug fix downld btn footer

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/acknowledgementResource/applicationSuccessFooter.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/acknowledgementResource/applicationSuccessFooter.js
@@ -270,7 +270,7 @@ export const applicationSuccessFooter = (
           }
         },
         children: {
-          downloadFormButtonLabel: (purpose === "pay" && type === "ESTATE_SERVICE_ESTATE_BRANCH.PROPERTY_MASTER") ? getLabel({
+          downloadFormButtonLabel: (purpose === "pay" && (type === "ESTATE_SERVICE_ESTATE_BRANCH.PROPERTY_MASTER" || type === "ESTATE_SERVICE_ESTATE_BRANCH.SECURITY_DEPOSIT" || type === "ESTATE_SERVICE_ESTATE_BRANCH.EXTENSIONFEE" || type === "ESTATE_SERVICE_ESTATE_BRANCH.PENALTY")) ? getLabel({
             labelName: "DOWNLOAD RECEIPT",
             labelKey: "ES_APPLICATION_BUTTON_DOWN_RECEIPT"
           }): getLabel({
@@ -371,7 +371,7 @@ export const applicationSuccessFooter = (
           }
         },
         children: {
-          printFormButtonLabel: (purpose === "pay" && type === "ESTATE_SERVICE_ESTATE_BRANCH.PROPERTY_MASTER") ? getLabel({
+          printFormButtonLabel: (purpose === "pay" && (type === "ESTATE_SERVICE_ESTATE_BRANCH.PROPERTY_MASTER" || type === "ESTATE_SERVICE_ESTATE_BRANCH.SECURITY_DEPOSIT" || type === "ESTATE_SERVICE_ESTATE_BRANCH.EXTENSIONFEE" || type === "ESTATE_SERVICE_ESTATE_BRANCH.PENALTY")) ? getLabel({
             labelName: "PRINT RECEIPT",
             labelKey: "ES_APPLICATION_BUTTON_PRINT_RECEIPT"
           }) : getLabel({
