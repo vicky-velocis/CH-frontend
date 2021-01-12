33fa14a092b1163a9cd50beb204a9727fd3d9805
Pooja Gawas <pooja_gawas@bitbucket.org>
2021-01-12 17:37:34 +0530

    changed query params to get only approved applications

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/owner-details.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/owner-details.js
@@ -380,7 +380,8 @@ const updateAllFields = async (action, state, dispatch) => {
           let ownerId = element.id;
           let queryObject = [
             { key: "ownerId", value: ownerId },
-            { key: "branchType", value: applicationBranchType }
+            { key: "branchType", value: applicationBranchType },
+            { key: "state", value: ESTATE_APPROVED_STATE }
           ]
           let payload = await getSearchApplicationsResults(queryObject);
           let modeOfTransferArr = [];
