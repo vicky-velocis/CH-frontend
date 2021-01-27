c362b16636df23ff1d677718d20b1da4c1b8a1df
kshitijzutshi <kshitij.zutshi@gmail.com>
2021-01-27 19:25:10 +0530

    PE-820: Show state as drafted when state is empty

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate-citizen/my-applications.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate-citizen/my-applications.js
@@ -247,6 +247,12 @@ const getData = async (action, state, dispatch) => {
   ]
   const response = await getSearchApplicationsResults(queryObject);
   if (!!response && !!response.Applications && !!response.Applications.length) {
+    // When BE response has state as "" Need to show drafted instead of NA
+    for (let i = 0; i < response.Applications.length; i++) {
+      if(response.Applications[i].state.length === 0){
+        response.Applications[i].state = "ES_DRAFTED"
+      } 
+    }
     dispatch(prepareFinalObject("actualResults", response.Applications));
     dispatch(prepareFinalObject("searchResults", response.Applications));
   }
