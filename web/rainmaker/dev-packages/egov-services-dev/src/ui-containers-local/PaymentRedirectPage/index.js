4c8ff02cd5f1f18aa867d36ecd41c85bff6feb69
sankalpDeveloper-react <sankalp.sharma@velocis.co.in>
2021-02-12 10:28:23 +0530

    fixed mail issue

--- web/rainmaker/dev-packages/egov-services-dev/src/ui-containers-local/PaymentRedirectPage/index.js
+++ web/rainmaker/dev-packages/egov-services-dev/src/ui-containers-local/PaymentRedirectPage/index.js
@@ -6,6 +6,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { getSearchResultsView } from "../../ui-utils/commons";
+import {
+    downloadReceipt,
+    downloadCertificate,
+    
+} from "../../ui-config/screens/specs/utils";
 
 class PaymentRedirect extends Component {
     componentDidMount = async () => {
@@ -79,19 +84,41 @@ class PaymentRedirect extends Component {
                       payload.bkToDate = payload.bkEndingDate;
                     }
                 }
-                response = await httpRequest(
+                
+               
+                let paymentReceipt= await downloadReceipt(payload, consumerCode, tenantId, 'true')
+                
+                
+                let permissionLetter= await downloadCertificate(payload, consumerCode, tenantId, 'true')
+                
+                Promise.all(paymentReceipt).then(data=>{
+                    let urlPayload={
+                        "paymentReceipt" :  data[0]
+                    }
+
+                    Promise.all(permissionLetter).then(permissionLetterData=>{
+
+                        urlPayload= {
+                            ...urlPayload, 
+                            "permissionLetter": permissionLetterData[0]
+                        }
+                        console.log(urlPayload, "payload")
+               
+                 httpRequest(
                     "post",
                     apiUrl,
                     "",
                     [],
-                    {
+                    { UrlData: urlPayload, 
                         Booking: payload,
                     }
                 );
                 this.props.setRoute(
                     `/egov-services/acknowledgement?purpose=${"pay"}&status=${"success"}&applicationNumber=${consumerCode}&tenantId=${tenantId}&secondNumber=${transactionId}&businessService=${bookingType}`
                 );
-            }
+                    })
+            })
+        }
         } catch (e) {
             console.log(e);
         }
